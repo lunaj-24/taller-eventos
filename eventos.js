ae52aacd130ae6875298238c19a92e50996fa874
Luna <luferr98@gmail.com>
2024-08-22 22:47:44 -0300

    Checkeado - Sitio web creado

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 function saludarOnClickDiv() {
     alert("Hola! Soy el div");
 }
+
+window.onload = function() {
+    // Configura el evento para el div
+    document.getElementById('elDiv').addEventListener('click', saludarOnClickDiv);
+}S
