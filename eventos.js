ba4f78c660db99461df0f715170b220f46289598
Luna Ferrizzo <luferr98@gmail.com>
2024-08-22 22:54:18 -0300

    eventos.js

--- eventos.js
+++ eventos.js
@@ -5,4 +5,4 @@ function saludarOnClickDiv() {
 window.onload = function() {
     // Configura el evento para el div
     document.getElementById('elDiv').addEventListener('click', saludarOnClickDiv);
-}S
+}
